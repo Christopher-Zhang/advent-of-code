6998e78cb7c8fb8e2a4e8b819e15f790f228d4b0
Christopher-Zhang <chrisconcord@gmail.com>
2025-12-24 22:25:50 -0500

    Day 6 part 2

---
+++ advent2025/src/day10.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+use itertools::Itertools;
+use crate::util::*;
+
+
+pub async fn advent_1(data: String, _test: bool) -> usize {
+    let mut total = 0;
+
+    total
+}
+
+pub async fn advent_2(data: String, _test: bool) -> usize {
+    let mut total = 0;
+
+    total
+}
